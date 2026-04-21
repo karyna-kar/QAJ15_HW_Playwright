bb09ddd7b8c03f823a8288148708c08dbd5f02b0
Karina <karyna.karmaza@gmail.com>
2026-04-21 19:57:10 +0300

    fix comments

--- tests/task_22/task3_test.spec.ts
+++ tests/task_22/task3_test.spec.ts
@@ -4,7 +4,8 @@ test.describe('Test herokuapp/hovers page', async () => {
   test('Verify hover method', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/hovers');
     await page.locator('.figure').first().hover();
-    expect(page.locator('.figcaption').first()).toBeVisible();
-    expect(page.locator('.figcaption').first()).toHaveText(/name: user1/, /View profile/);
+    const image = page.locator('.figcaption').first();
+    expect(image).toBeVisible();
+    expect(image).toHaveText(/name: user1/, /View profile/);
   });
 });
