8a34e204dbb58183201eca5706a11646358cfa20
Karina <karyna.karmaza@gmail.com>
2026-05-03 12:05:43 +0300

    add elements

--- tests/task_25/page_object/elements/footer.ts
+++ tests/task_25/page_object/elements/footer.ts
@@ -3,17 +3,17 @@ import { Locator, Page } from '@playwright/test';
 export class Footer {
   readonly footerContainer: Locator;
   readonly socialIconLinks: Locator;
-  readonly socialIconTwitter:Locator;
-  readonly socialIconFacebook:Locator;
-  readonly socialIconlinkedin:Locator;
-  readonly copyRightText:Locator;
+  readonly socialIconTwitter: Locator;
+  readonly socialIconFacebook: Locator;
+  readonly socialIconlinkedin: Locator;
+  readonly copyRightText: Locator;
 
   constructor(page: Page) {
     this.footerContainer = page.locator('.footer');
-    this.socialIconLinks = this.footerContainer.locator('.social');
-    this.socialIconTwitter = this.socialIconLinks.locator('.social_twitter');
-    this.socialIconFacebook = this.socialIconLinks.locator('.social_facebook');
-    this.socialIconlinkedin = this.socialIconLinks.locator('.social_linkedin');
+    this.socialIconLinks = this.footerContainer.locator('.social a');
+    this.socialIconTwitter = this.footerContainer.locator('[data-test="social-twitter"]');
+    this.socialIconFacebook = this.footerContainer.locator('[data-test="social-facebook"]');
+    this.socialIconlinkedin = this.footerContainer.locator('[data-test="social-linkedin"]');
     this.copyRightText = this.footerContainer.locator('.footer_copy');
   }
 }
