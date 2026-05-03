fd0d79019cd7d632fecd668a30ba2639125bc24f
Karina <karyna.karmaza@gmail.com>
2026-05-03 12:05:54 +0300

    add tests

---
+++ tests/task_25/inventory_page.spec.ts
@@ -0,0 +1,64 @@
+import { expect } from '@playwright/test';
+import { test } from '../../fixtures/custome-fixtures';
+
+test.describe('Inventory Page Tests', async () => {
+  test.beforeEach(async ({ swagLabs, loginStandartUser }) => {
+    await swagLabs.inventoryPage.navigate();
+  });
+
+  test('Verify header', async ({ swagLabs }) => {
+    await expect(swagLabs.inventoryPage.header.appLogo).toHaveText('Swag Labs');
+    await swagLabs.inventoryPage.header.burgerMenu.openMenu();
+    expect(swagLabs.inventoryPage.header.burgerMenu.burgerMenuContainer).toBeVisible();
+    await swagLabs.inventoryPage.header.burgerMenu.closeMenu();
+    expect(swagLabs.inventoryPage.header.burgerMenu.burgerMenuContainer).toBeHidden();
+
+    await swagLabs.inventoryPage.addRandomItemToTheCart();
+    await expect(swagLabs.inventoryPage.header.shoppingCartCounter).toHaveText('1');
+  });
+
+  test('Verify footer', async ({ swagLabs }) => {
+    await expect(swagLabs.inventoryPage.footer.socialIconLinks).toHaveCount(3);
+
+    //have correct URls
+    await expect(swagLabs.inventoryPage.footer.socialIconTwitter).toHaveAttribute('href', /twitter/);
+    await expect(swagLabs.inventoryPage.footer.socialIconFacebook).toHaveAttribute('href', /facebook/);
+    await expect(swagLabs.inventoryPage.footer.socialIconlinkedin).toHaveAttribute('href', /linkedin/);
+  });
+
+  test('Verify title', async ({ swagLabs }) => {
+    await expect(swagLabs.inventoryPage.pageTitle).toHaveText('Products');
+  });
+
+  test('Verify number of items', async ({ swagLabs }) => {
+    await expect(swagLabs.inventoryPage.inventoryItems).toHaveCount(6);
+  });
+
+  test('Verify logout', async ({ swagLabs }) => {
+    await swagLabs.inventoryPage.logOut();
+    expect(swagLabs.loginPage.url).toBe('https://www.saucedemo.com/');
+    await expect(swagLabs.loginPage.userNameInput).toBeEmpty();
+    await expect(swagLabs.loginPage.passwordInput).toBeEmpty();
+  });
+
+  test('Verify adding item', async ({ swagLabs }) => {
+    const randomItem = await swagLabs.inventoryPage.addRandomItemToTheCart();
+    const removeButton = await swagLabs.inventoryPage.getItemRemoveButton(randomItem);
+    await expect(removeButton).toBeVisible();
+  });
+
+  test('Verify deleting item', async ({ swagLabs }) => {
+    const randomItem = await swagLabs.inventoryPage.addRandomItemToTheCart();
+    const removeButton = await swagLabs.inventoryPage.getItemRemoveButton(randomItem);
+    await removeButton.click();
+    const addButton = await swagLabs.inventoryPage.getItemAddButton(randomItem);
+    await expect(addButton).toBeVisible();
+  });
+
+  test('Open Item page', async ({ swagLabs }) => {
+    await swagLabs.inventoryPage.logOut();
+    expect(swagLabs.loginPage.url).toBe('https://www.saucedemo.com/');
+    await expect(swagLabs.loginPage.userNameInput).toBeEmpty();
+    await expect(swagLabs.loginPage.passwordInput).toBeEmpty();
+  });
+});
--- tests/task_25/login_page.spec.ts
+++ tests/task_25/login_page.spec.ts
@@ -1,17 +1,30 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from './page_object/login_page';
+import { expect } from '@playwright/test';
+import { test } from '../../fixtures/custome-fixtures';
 
 test.describe('Login Page Tests', async () => {
-  let loginPage : LoginPage;
-
-  test.beforeEach(async ({ page }) => {
-    loginPage = new LoginPage(page);
-    loginPage.navigate();
+  test.beforeEach(async ({ swagLabs }) => {
+    await swagLabs.loginPage.navigate();
   });
 
-  test('Verify mandatory inputs', async ({}) => {
-    await loginPage.loginButton.click();
-    await expect(loginPage.errorMessage).toBeVisible();
-    await expect(loginPage.errorMessage).toHaveText('Epic sadface: Username is required');
+  test('Verify mandatory inputs: Username', async ({ swagLabs }) => {
+    await swagLabs.loginPage.passwordInput.fill('test');
+    await swagLabs.loginPage.loginButton.click();
+    await expect(swagLabs.loginPage.errorMessage).toBeVisible();
+    await expect(swagLabs.loginPage.errorMessage).toHaveText('Epic sadface: Username is required');
+  });
+
+  test('Verify mandatory inputs: Password', async ({ swagLabs }) => {
+    await swagLabs.loginPage.userNameInput.fill('test');
+    await swagLabs.loginPage.loginButton.click();
+    await expect(swagLabs.loginPage.errorMessage).toBeVisible();
+    await expect(swagLabs.loginPage.errorMessage).toHaveText('Epic sadface: Password is required');
+  });
+
+  test('Verify login with invalid credentials', async ({ swagLabs }) => {
+    await swagLabs.loginPage.userNameInput.fill('test');
+    await swagLabs.loginPage.passwordInput.fill('test');
+    await swagLabs.loginPage.loginButton.click();
+    await expect(swagLabs.loginPage.errorMessage).toBeVisible();
+    await expect(swagLabs.loginPage.errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service');
   });
 });
