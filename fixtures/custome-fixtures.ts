200c4b36ef9deaa644e6cc48c8fb477e14a14504
Karina <karyna.karmaza@gmail.com>
2026-05-05 15:13:34 +0300

    add custome_fixtures

--- fixtures/custome-fixtures.ts
+++ fixtures/custome-fixtures.ts
@@ -1,9 +1,13 @@
 import { test as baseTest } from '@playwright/test';
+import { RestfulController } from '../tests/task_24/api-tests/controller-api';
 import 'dotenv/config';
 
 interface ExtendedFicture {
   loginStandartUser: undefined;
   addItemsToCard: number;
+  restfulControllerAuthorizedUser: RestfulController;
+  restfulControllerNotAuthorizedUser: RestfulController;
+  restfulControllerInvalidApiKey: RestfulController;
 }
 
 export const test = baseTest.extend<ExtendedFicture>({
@@ -28,5 +32,25 @@ export const test = baseTest.extend<ExtendedFicture>({
       localStorage.setItem('cart-contents', JSON.stringify(items));
     }, addingItemsIds);
     await use(addingItemsIds.length);
+  },
+
+  restfulControllerAuthorizedUser: async ({ playwright }, use) => {
+    const baseURL = 'https://api.restful-api.dev';
+    const authRequest = await playwright.request.newContext({ extraHTTPHeaders: { 'x-api-key': process.env.API_KEY as string } });
+    const controller = new RestfulController(authRequest, baseURL);
+    await use(controller);
+  },
+
+  restfulControllerNotAuthorizedUser: async ({ request }, use) => {
+    const baseURL = 'https://api.restful-api.dev';
+    const controller = new RestfulController(request, baseURL);
+    await use(controller);
+  },
+
+  restfulControllerInvalidApiKey: async ({ playwright }, use) => {
+    const baseURL = 'https://api.restful-api.dev';
+    const authRequest = await playwright.request.newContext({ extraHTTPHeaders: { 'x-api-key': 'Test' } });
+    const controller = new RestfulController(authRequest, baseURL);
+    await use(controller);
   }
 });
